357aaeef9c41b7035c0a15f90876fbc563b9cdba
sarvani29 <sarvani009@gmail.com>
2020-07-27 03:32:06 +0100

    update reading of files

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from maxprofit import MaxProfit
 
 data = pd.read_csv(sys.argv[1], error_bad_lines=False)
 arr1 = data.columns[1:]
+
 for col in arr1:
     arr = data[col].to_numpy()
     solution = MaxProfit.maxProfit(arr)
     print("The maximum profit for " + col + " is: ", solution)
+
 
